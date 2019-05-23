eb8d0c1367a2e01365f8ec65cc0384bef3d0fe8f
betttyquu <absheng947@hotmail.com>
2019-05-23 16:58:42 +0000

    move domain and apikey into .env

--- mailgun.js
+++ mailgun.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const mailgun = require("mailgun-js");
-const DOMAIN = 'sandbox979fff432805483a88a58853cedc3fa4.mailgun.org';
-const mg = mailgun({apiKey: '737b5a742b2dd75c2b2d6f2a3c53fe1f-52b0ea77-2c6e0f78', domain: DOMAIN});
+const DOMAIN = process.env.DOMAIN;
+const mg = mailgun({apiKey: process.env.API_KEY, domain: DOMAIN});
 const data = {
-	from: 'Polly <me@sandbox979fff432805483a88a58853cedc3fa4.mailgun.org>',
-	to: 'absheng947@hotmail.com',
+	from: `Pollr <me@${DOMAIN}>`,
+	to: 'betttyquu@gmail.com',
 	subject: 'Hello2',
 	text: 'Testing some Mailgun awesomness!'
 };
 mg.messages().send(data, function (error, body) {
+	if (error) {
+		console.log(error);
+	}
 	console.log(body);
 });
