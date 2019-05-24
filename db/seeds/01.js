893ff5b500b78ad722624546b8623ff4d5859452
nelsonyu <nelson.tm.yu@gmail.com>
2019-05-24 05:10:53 +0000

    Added more seed data

--- db/seeds/01.js
+++ db/seeds/01.js
@@ -2,11 +2,32 @@ exports.seed = function(knex, Promise) {
     return knex('polls').del()
       .then(function () {
         return Promise.all([
-          knex('polls').insert({id: 1, admin_name: 'Alice', admin_email: 'example@email.com', question: 'what', vote_link: "vote1.link", result_link: "result1.link"}),
-          knex('polls').insert({id: 2, admin_name: 'Bob',admin_email: 'example2@email.com',question: 'how', vote_link: "vote2.link", result_link: "result2.link"}),
-          knex('polls').insert({id: 3, admin_name: 'Charlie',admin_email: 'example3@email.com',question: 'wow', vote_link: "vote4.link", result_link: "result1.link"})
+          knex('polls').insert({
+            id: 1,
+            admin_name: 'Betty',
+            admin_email: 'example@email.com',
+            question: 'What do you want for dinner tonight?',
+            vote_link: "localhost:8080/vote/1",
+            result_link: "localhost:8080/result/1"
+          }),
+          knex('polls').insert({
+            id: 2,
+            admin_name: 'Nelson',
+            admin_email: 'example2@email.com',
+            question: 'Who is your favourite instructor?',
+            vote_link: "localhost:8080/vote/2",
+            result_link: "localhost:8080/result/2"
+          }),
+          knex('polls').insert({
+            id: 3,
+            admin_name: 'Turner',
+            admin_email: 'example3@email.com',
+            question: 'When should I have lunch?',
+            vote_link: "localhost:8080/vote/3",
+            result_link: "localhost:8080/vote/3"
+          })
         ]);
       });
   };
-  
-  
+
+
